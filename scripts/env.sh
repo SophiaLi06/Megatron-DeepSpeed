3bef254dc97d153b1ca1f5ef303fd633bcb0394f
SophiaLi06 <minghaoli@g.harvard.edu>
2025-03-14 12:48:57 -0400

    add environmental variable script

---
+++ scripts/env.sh
@@ -0,0 +1,9 @@
+export PYTORCH_IMAGE=nvcr.io/nvidia/pytorch:24.01-py3
+export CHECKPOINT_PATH="new_checkpoints"
+export TENSORBOARD_LOGS_PATH="workspace/logs"
+export VOCAB_FILE="dataset/gpt/gpt2-vocab.json"
+export MERGE_FILE="dataset/gpt/gpt2-merges.txt"
+export DATA_PATH="dataset/gpt/my-gpt2_text_document"
+export NCCL_SOCKET_IFNAME=enp37s0f0np0
+export GLOO_SOCKET_IFNAME=enp37s0f0np0
+export MASTER_PORT=5000
