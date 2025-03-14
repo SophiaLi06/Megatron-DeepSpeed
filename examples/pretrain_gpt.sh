8528f19f7c41c14968c0a8c398a9109475561e8e
SophiaLi06 <minghaoli@g.harvard.edu>
2025-03-14 15:10:53 -0400

    add deepspeed args

--- examples/pretrain_gpt.sh
+++ examples/pretrain_gpt.sh
@@ -25,6 +25,16 @@ VOCAB_FILE=$3 #<Specify path to file>/gpt2-vocab.json
 MERGE_FILE=$4 #<Specify path to file>/gpt2-merges.txt
 DATA_PATH=$5 #<Specify path and file prefix>_text_document
 
+DS_CONFIG=ds_configs/gpt_ds_config.json
+ZERO_STAGE=2
+
+ds_args=""
+ds_args=" --deepspeed ${ds_args}"
+ds_args=" --no-pipeline-parallel ${ds_args}" 
+ds_args=" --deepspeed_config=$DS_CONFIG ${ds_args}"
+ds_args=" --zero-stage=$ZERO_STAGE ${ds_args}"
+ds_args=" --deepspeed-activation-checkpointing ${ds_args}"
+
 GPT_ARGS="
     --num-layers 24 \
     --hidden-size 1024 \
@@ -32,7 +42,7 @@ GPT_ARGS="
     --seq-length 1024 \
     --max-position-embeddings 1024 \
     --micro-batch-size 4 \
-    --global-batch-size 8 \
+    --global-batch-size 64 \
     --lr 0.00015 \
     --train-iters 500 \
     --lr-decay-iters 320000 \
@@ -63,5 +73,6 @@ torchrun pretrain_gpt.py \
     $GPT_ARGS \
     $DATA_ARGS \
     $OUTPUT_ARGS \
+    $ds_args \
     --save $CHECKPOINT_PATH \
     --load $CHECKPOINT_PATH
