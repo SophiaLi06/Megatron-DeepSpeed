303be6ea7e4fa994977274fffe18a09b8aca97a0
SophiaLi06 <minghaoli@g.harvard.edu>
2025-03-14 17:40:59 -0400

    update distributed gpt script

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -1,21 +1,76 @@
 #!/bin/bash
 
+RECREATE_CHECKPOINTS=0
+# add an "-d" option to delete the previous checkpoint
+while getopts "d" opt; do
+  case ${opt} in
+    d )
+      RECREATE_CHECKPOINTS=1
+      ;;
+    \? )
+      echo "Usage: cmd [-d]"
+      ;;
+  esac
+done
+
+# clear the options
+shift $((OPTIND -1))
+
 # Runs the "345M" parameter model
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-GPUS_PER_NODE=8
+GPUS_PER_NODE=1
 # Change for multinode config
-MASTER_ADDR=localhost
-MASTER_PORT=6000
-NNODES=1
-NODE_RANK=0
+MASTER_ADDR=10.1.1.3
+MASTER_PORT=$7
+NNODES=2
+NODE_RANK=$6
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-CHECKPOINT_PATH=<Specify path>
-VOCAB_FILE=<Specify path to file>/gpt2-vocab.json
-MERGE_FILE=<Specify path to file>/gpt2-merges.txt
-DATA_PATH=<Specify path and file prefix>_text_document
+CHECKPOINT_PATH=$1 #<Specify path>
+TENSORBOARD_LOGS_PATH=$2 #<Specify path>
+VOCAB_FILE=$3 #<Specify path to file>/gpt2-vocab.json
+MERGE_FILE=$4 #<Specify path to file>/gpt2-merges.txt
+DATA_PATH=$5 #<Specify path and file prefix>_text_document
+
+if [ $RECREATE_CHECKPOINTS -eq 1 ]; then
+    echo "Deleting previous checkpoint"
+    rm -rf $CHECKPOINT_PATH
+    mkdir $CHECKPOINT_PATH
+fi
+
+DS_CONFIG="./examples/ds_configs/gpt_ds_config.json"
+GLOBAL_BATCH=64
+MICRO_BATCH=4
+ZERO_STAGE=2
+
+cat <<EOT > $DS_CONFIG
+{
+  "train_batch_size" : $GLOBAL_BATCH,
+  "train_micro_batch_size_per_gpu": $MICRO_BATCH,
+  "steps_per_print": 100,
+
+  "zero_optimization": {
+    "stage": $ZERO_STAGE
+  },
+
+  "fp16": {
+    "enabled": true,
+    "initial_scale_power": 12
+  },
+
+  "wall_clock_breakdown" : true
+}
+EOT
+
+ds_args=""
+ds_args=" --deepspeed ${ds_args}"
+ds_args=" --no-pipeline-parallel ${ds_args}" 
+ds_args=" --deepspeed_config=$DS_CONFIG ${ds_args}"
+ds_args=" --zero-stage=$ZERO_STAGE ${ds_args}"
+ds_args=" --deepspeed-activation-checkpointing ${ds_args}"
+ds_args=" --no-persist-layer-norm ${ds_args}"
 
 DISTRIBUTED_ARGS="
     --nproc_per_node $GPUS_PER_NODE \
@@ -31,10 +86,10 @@ GPT_ARGS="
     --num-attention-heads 16 \
     --seq-length 1024 \
     --max-position-embeddings 1024 \
-    --micro-batch-size 8 \
-    --global-batch-size 64 \
+    --micro-batch-size $MICRO_BATCH \
+    --global-batch-size $GLOBAL_BATCH \
     --lr 0.00015 \
-    --train-iters 500000 \
+    --train-iters 500 \
     --lr-decay-iters 320000 \
     --lr-decay-style cosine \
     --min-lr 1.0e-5 \
@@ -63,6 +118,7 @@ torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
     $GPT_ARGS \
     $DATA_ARGS \
     $OUTPUT_ARGS \
+    $ds_args \
     --distributed-backend nccl \
     --save $CHECKPOINT_PATH \
     --load $CHECKPOINT_PATH
