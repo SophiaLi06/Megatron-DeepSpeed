cef1e2f3fbc3516411b058bf6fb23c5cceb378c5
SophiaLi06 <minghaoli@g.harvard.edu>
2025-03-24 23:39:58 -0400

    zero1 with larger mbs

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -46,8 +46,8 @@ if [ $RECREATE_CHECKPOINTS -eq 1 ]; then
 fi
 
 DS_CONFIG="./examples/ds_configs/gpt_ds_config.json"
-GLOBAL_BATCH=128
-MICRO_BATCH=4
+GLOBAL_BATCH=64
+MICRO_BATCH=8
 ZERO_STAGE=1
 
 cat <<EOT > $DS_CONFIG
